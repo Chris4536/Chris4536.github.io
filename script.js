e8a9b1a2fdba471528bcd9e0e4ff5ffc52e8c1a0
Chris4536 <59596505+Chris4536@users.noreply.github.com>
2021-04-28 11:29:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,13 @@
-
-
-firebase.auth().createUserWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    // Signed in 
-    var user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ..
-  });
-
-firebase.auth().signInWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    // Signed in
-    var user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-  });
-
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // signed in.
-  } else {
-    // no sign in.
-  }
-});
-
-firebase.auth().signOut()
-  .then(function() {
-    // Sign-out successful.
-  })
-  .catch(function(error) {
-    // An error happened
-  });
-
-function loadChannels() {
-  database.collection("channels").orderBy("name")
-}
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDhzDUw_ukDp17V2aXXpH_Wu54Xzxr68CE",
+  authDomain: "chatapper-32e7c.firebaseapp.com",
+  projectId: "chatapper-32e7c",
+  storageBucket: "chatapper-32e7c.appspot.com",
+  messagingSenderId: "500187557839",
+  appId: "1:500187557839:web:c94de8f9bfe1ff54032cba",
+  measurementId: "G-QME7PVT2KG"
+};
 
 function openForm() {
   document.getElementById("myForm").style.display = "block";
