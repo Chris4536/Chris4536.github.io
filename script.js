438c7108f94feff2a6765aa508f16849fdf2139e
Chris4536 <59596505+Chris4536@users.noreply.github.com>
2021-05-03 22:49:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-var feedbackHTML = document.getElementById("feedback");
-var authenticationHTML = document.getElementById("authentication");
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyDhzDUw_ukDp17V2aXXpH_Wu54Xzxr68CE",
+  authDomain: "chatapper-32e7c.firebaseapp.com",
+  projectId: "chatapper-32e7c",
+  storageBucket: "chatapper-32e7c.appspot.com",
+  messagingSenderId: "500187557839",
+  appId: "1:500187557839:web:c94de8f9bfe1ff54032cba",
+  measurementId: "G-QME7PVT2KG"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
+var database = firebase.firestore();
+
+var feedbackHTML = document.getElementById("feedback");
+var authenticationHTML = document.getElementById("screen-authentication");
+var chatScreenHTML = document.getElementById("screen-chat");
+var messagesHTML = document.getElementById("messages");
+
+loadDefaultValues();
+
+function loadDefaultValues() {
+  document.getElementById("email").value = "Chris@blah.com";
+  document.getElementById("password").value = "CheekyBreeki123"
+}
 
 function signup() {
 
@@ -11,7 +36,7 @@ function signup() {
   firebase.auth().createUserWithEmailAndPassword(email, password)
     .then((userCredential) => {
       feedbackHTML.innerHTML = "Account created";
-      hideAuthentication();
+      showChatScreen();
   })
   .catch((error) => {
     feedbackHTML.innerHTML = error.message;
@@ -26,7 +51,7 @@ function login() {
   firebase.auth().signInWithEmailAndPassword(email, password)
   .then((userCredential) => {
     feedbackHTML.innerHTML = "Login success";
-    hideAuthentication();
+    showChatScreen();
   })
   .catch((error) => {
     feedbackHTML.innerHTML = error.message;
@@ -35,14 +60,105 @@ function login() {
 
 function showAuthentication() {
   authenticationHTML.style.display = "block";
-
+  chatScreenHTML.style.display = "none";
 }
 
-function hideAuthentication() {
+function showChatScreen() {
   authenticationHTML.style.display = "none";
+  chatScreenHTML.style.display = "block";
+  loadMessages();
+}
+
+function sendMessage() {
+  var message = document.getElementById("message-input").value;
+  
+  createMessageHTML(message);
+  
+
+
+
+
+ var message_dictionary = {
+   "message": message,
+   "user" : firebase.auth().currentUser.email,
+   "timestamp": firebase.firestore.FieldValue.serverTimestamp()
+ }
+ 
+  database.collection("channels").doc("OAoVpy03qLBJkjdSz4RB")
+  .collection("messages").add(message_dictionary);
+    then((docRef) => {
+      
+      document.getElementById("message-input").value = "";
+    })
+    .catch((error) => {
+      console.error("Error adding document: ", error.message);
+    });
+
+  
+}
+
+function createMessageHTML(message, user, timestamp) {
+  
+  var messageContainerHTML = document.createElement("div");
+  messageContainerHTML.classList.add("message-container");
+  
+  var messageHTML = document.createElement("p");
+  messageHTML.innerHTML = message;
+  messagesHTML.appendChild(messageHTML);
+
+  if (user) {
+
+    var userHTML = document.createElement("p");
+    userHTML.innerHTML = message;
+    messageContainerHTML.appendChild(userHTML);
+  }
+ 
+  if (timestamp) {
+
+    var timestampHTML = document.createElement("p");
+    timestampHTML.innerHTML = formatDate(timestamp.toDate());
+    messageContainerHTML.appendChild(timestampHTML);
+  }
+
+  messagesHTML.appendChild(messageContainerHTML);
+
 }
 
 
+function loadMessages(){
+  database.collection("channels").doc("OAoVpy03qLBJkjdSz4RB")
+  .collection("messages").orderBy("timestamp", "desc")
+      .onSnapshot((querySnapshot) => {
+        messagesHTML.innerHTML = ""
+      
+        querySnapshot.forEach((doc) => {
+			  	createMessageHTML(doc.data().message, doc.data().user, doc.data
+          ().timestamp);
+      
+      });
+    });
+   
+}
+
+
+
+document.addEventListener("keypress", function (event) {
+  var key = event.keyCode || event.which;
+  if (event.keyCode == 13) {
+    sendMessage();
+  }
+});
+
+function formatDate(date) {
+  hours = turnIntoTwoDigits(date.getHours());
+  minutes = turnIntoTwoDigits(date.getMinutes());
+  //seconds = turnIntoTwoDigits(date.getSeconds());
+  return hours + ":" + minutes; // + ":" + seconds;
+}
+
+function turnIntoTwoDigits(n) {
+  return n < 10 ? '0' + n : n;
+}
 
 
 
